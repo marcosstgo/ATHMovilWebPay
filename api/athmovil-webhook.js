77fcec1837b3c73a76bb52ea059c4e2fe0d086cd
Marcos Santiago <hello@marcossantiago.com>
2024-10-30 00:45:23 -0400

    Update

--- api/athmovil-webhook.js
+++ api/athmovil-webhook.js
@@ -1,18 +1,20 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
     if (req.method === 'POST') {
         const data = req.body;
+
         console.log('Evento recibido:', data);
 
-        if (data.eventName === 'payment_completed') {
-            console.log('Pago completado:', data);
-        } else if (data.eventName === 'payment_canceled') {
-            console.log('Pago cancelado:', data);
-        } else if (data.eventName === 'payment_expired') {
-            console.log('Pago expirado:', data);
+        if (data.eventName === 'payment.received') {
+            console.log(`Pago recibido: ${data.payment.paymentId}`);
+        } else if (data.eventName === 'payment.expired') {
+            console.log('El pago expiró.');
+        } else if (data.eventName === 'payment.canceled') {
+            console.log('El pago fue cancelado.');
         }
 
-        res.status(200).send('Evento recibido');
+        res.status(200).json({ message: 'Evento procesado correctamente' });
     } else {
-        res.status(405).send('Método no permitido');
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Método ${req.method} no permitido`);
     }
 }
